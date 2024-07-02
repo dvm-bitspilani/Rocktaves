2b26929eb7073b0f16d43b53a578fb9d5c9c1d91
Samyak Jain <jainsamyak1203@gmail.com>
2024-07-02 23:57:29 +0530

    home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import * as styles from "./Home.module.scss"
+
+export default function Home(){
+    return(
+        <>
+            <section className={styles.wrapper}>
+                <div className={styles.bg}></div>
+                <div className={styles.header}>
+                    <div className={styles.title}>
+                        <div className={styles.main}>rocktaves</div>
+                        <div className={styles.main}>rocktaves</div>
+                        <div className={styles.main}>rocktaves</div>
+                    </div>
+                    {/* <div className={styles.navbar}>
+                        <div className={styles.nav}>about</div>
+                        <div className={styles.nav}>register</div>
+                        <div className={styles.nav}>contact</div>
+                    </div> */}
+                </div>
+            </section>
+        </>
+    );
+}
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,10 @@
+import * as styles from "./LandingPage.module.scss";
+import Home from "./Home";
+
+export default function LandingPage(){
+    return(
+        <>
+            <Home />
+        </>
+    );
+}
