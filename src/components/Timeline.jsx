53940f8ddfd534115b6e055e980493d0062ee1a7
Samyak Jain <jainsamyak1203@gmail.com>
2024-07-03 16:42:28 +0530

    feat: added all steps

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -25,7 +25,22 @@ export default function Timeline() {
                         <div className={styles.text}>Each band is expected to perform live for <span>15-20 minutes</span> at their <span>chosen city's venue</span>. One winner from each city and one from each of the two online divisions will qualify for the final round that is held at <span>bits pilani</span></div>
                     </div>
                 </div>
-                
+            </section>
+            <section className={styles.wrapper}>
+                <div className={styles.bg2}></div>
+                <div className={styles.content}>
+                    <div className={styles.container}>
+                        <div className={styles.header}>
+                            step-3
+                            <span className={styles.final}>-finals at </span>
+                            <span className={styles.oasis}>
+                                <span className={styles.main}>oasis'24</span>
+                                <span className={styles.main}>oasis'24</span>
+                            </span>
+                        </div>
+                        <div className={styles.text}>Bands that qualified will be invited to play at Oasis, the annual cultural fest of BITS Pilani and the winning band will walk away with the title of the Rocktaves winner at the <span>52nd Oasis</span> along with the prizes which will be  revealed shortly.</div>
+                    </div>
+                </div>
             </section>
         </>
     );
