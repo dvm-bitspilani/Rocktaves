bb6fcf37a1dd68d4492be2cb26737fd072eac6b3
Samyak Jain <jainsamyak1203@gmail.com>
2024-07-07 13:15:59 +0530

    fix: loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,63 @@ import LandingPage from "./components/LandingPage";
 import "./App.css";
 import Loader from "./components/Loader";
 import { useEffect, useState } from "react";
+import indianocean from "./assets/indianocean.png"
+import prestorika from "./assets/prestorika.png"
+import parikrama from "./assets/parikrama.png"
+import themclones from "./assets/themclones.png"
+import pic1 from "./assets/pic1.png"
+import pic2 from "./assets/pic2.png"
+import pic3 from "./assets/pic3.png"
+import bg1 from "./assets/backgrounds/about.png"
+import bg2 from "./assets/backgrounds/contact.png"
+import bg3 from "./assets/backgrounds/home.png"
+import bg4 from "./assets/backgrounds/rules.png"
+import bg5 from "./assets/backgrounds/timeline1.png"
+import bg6 from "./assets/backgrounds/timeline2.png"
+import bg7 from "./assets/backgrounds/winners.png"
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true); // Initial loading state
 
   useEffect(() => {
-    const fakeDataFetch = () => {
-      setTimeout(() => {
+    const assets = [
+      indianocean,
+      prestorika,
+      parikrama,
+      themclones,
+      pic1,
+      pic2,
+      pic3,
+      bg1,
+      bg2,
+      bg3,
+      bg4,
+      bg5,
+      bg6,
+      bg7,
+    ];
+
+    const load = async () => {
+      try {
+        await Promise.all(
+          assets.map(
+            (asset) =>
+              new Promise((resolve) => {
+                const img = new Image();
+                img.src = asset;
+                img.onload = resolve;
+                img.onerror = resolve;
+              })
+          )
+        );
         setIsLoading(false);
-      }, 4000);
+      } catch (error) {
+        console.error("Error loading assets:", error);
+        setIsLoading(false);
+      }
     };
 
-    fakeDataFetch();
+    load();
   }, []);
 
   return isLoading ? (
